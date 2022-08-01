9ef0a28cad50d386106927ca22c0701a2764c2c9
GoluchinMichail <109588187+GoluchinMichail@users.noreply.github.com>
2022-08-01 15:06:43 +0300

    Update test_1.c

--- test_1.c
+++ test_1.c
@@ -9,18 +9,32 @@
 #include <arpa/inet.h>
 #include <ev.h>
 
-char acBuffer [1024];
+static char *strrev (char *p) {
+    // исключить EOL-символы
+    int length = strcspn (p, "\r\n");
+    if (length) {
+        int c, i, j;
+        for (i=0, j=length - 1; i < j; i++, j--) {
+            c = p[i];
+            p[i] = p[j];
+            p[j] = c;
+        }
+    }
 
-int socket_init (int iPort) {
+    return p;
+}
+
+int sock_bind_listen (int iPort) {
     struct sockaddr_in my_addr;
     int listener;
+
     if ((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror ("socket");
-        exit (1);
+        return -1;
     } else
         printf ("SOCKET CREATE SUCCESS!\n");
 
-    int so_reuseaddr=1;
+    int so_reuseaddr = 1;
     setsockopt (listener,SOL_SOCKET,SO_REUSEADDR,&so_reuseaddr,sizeof(so_reuseaddr));
     memset (&my_addr, 0, sizeof(my_addr));
     my_addr.sin_family = PF_INET;
@@ -29,76 +43,56 @@ int socket_init (int iPort) {
  
     if (bind(listener, (struct sockaddr *) &my_addr, sizeof(struct sockaddr))== -1) {
         perror ("bind error!\n");
-        exit (1);
+        return -2;
     } else
         puts ("IP BIND SUCCESS,IP: 127.0.0.1");
  
     if (listen(listener, 1024) == -1) {
         perror ("listen error!\n");
-        exit (1);
+        return -3;
     } else
         printf ("LISTEN SUCCESS, PORT: %d\n\n", iPort);
 
     return listener;
 }
 
-void recv_callback (struct ev_loop *evLoop, ev_io *w, int );
+void recv_cb (struct ev_loop *evLoop, ev_io *w, int ) {
 
-void write_callback (struct ev_loop *evLoop, ev_io *w, int ) {
-    write (w->fd,acBuffer,strlen(acBuffer));
-
-    ev_io_stop (evLoop,  w);
-    ev_io_init (w,recv_callback,w->fd,EV_READ);
-    ev_io_start (evLoop,w);
-}
-
-void recv_callback (struct ev_loop *evLoop, ev_io *w, int ) {
-
-    int ret = recv (w->fd,acBuffer,sizeof(acBuffer),0);
+    char acBuffer [1024];
+    int ret = recv (w->fd,acBuffer,sizeof(acBuffer)-1,0);
     if (ret > 0) {
-        acBuffer[ret] = 0;
+        acBuffer [ret] = 0;
         printf (acBuffer);
-
-        // послать ответ ... тот что в  prm->acBuffer
-        ev_io_stop (evLoop,  w);
-        ev_io_init (w, write_callback, w->fd, EV_WRITE);
-        ev_io_start (evLoop, w);
-
+        write (w->fd, strrev(acBuffer),strlen(acBuffer));
+    } else if ((ret < 0) && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+        ;
     } else {
         puts ("remote socket closed!");
+        close (w->fd);
+        // отменить текущий обработчик
+        ev_break (evLoop, EVBREAK_CANCEL);
+        // и освободить ресурс
         ev_io_stop (evLoop, w);
-
-        // как ещё остановить это - итио ^_^     извините :)
-        ev_io_init (w,NULL,0,0);
-        ev_io_stop (evLoop, w);
-
+        free (w);
     }
 
 }
 
-void accept_callback (struct ev_loop *evLoop, ev_io *w, int ) {
-    int newfd;
-    struct sockaddr_in sin;
-    socklen_t addrlen = sizeof(struct sockaddr);
+void accept_cb (struct ev_loop *evLoop, ev_io *w, int ) {
+    int newSock;
 
-    while ((newfd = accept(w->fd, (struct sockaddr *)&sin, &addrlen)) < 0) {
-        if (errno == EAGAIN || errno == EWOULDBLOCK) {
-            continue; 
-        } else {
-            printf ("accept error.[%s]\n", strerror(errno));
-            break;
-        }
+    if ((newSock = accept(w->fd, NULL, 0)) > 0) {
+        // добавить новый обработчик
+        ev_io *new_client = calloc (1, sizeof(*new_client));
+        ev_io_init (new_client, recv_cb, newSock, EV_READ);
+        ev_io_start (evLoop, new_client);
+        puts ("new client ...");
     }
-
-    ev_io_stop (evLoop,w);
-    ev_io_init (w,recv_callback,newfd,EV_READ);
-    ev_io_start (evLoop,w);
-
-    printf("accept callback : fd :%d\n",w->fd);
 }
 
 int main (int argc ,char** argv) {
     if (argc < 2) {
+Help:
         printf ("\n"
 "Usage:\n"
 "   %s num_port\n"
@@ -106,21 +100,27 @@ int main (int argc ,char** argv) {
         return 1;
     }
 
-    int iSockListen = socket_init (atoi(argv[1]));
+    int port = atoi (argv[1]);
+    if (! (port && port<=65535) ) {
+        puts ("\n-ERROR port\n");
+        goto Help;
+    }
+    
+    int iSockListen = sock_bind_listen (port);
+    if (iSockListen > 0) {
+        struct ev_loop *evloop = ev_default_loop (EVFLAG_NOENV);
+        ev_io *watcher = calloc (1, sizeof(*watcher));
+        if (evloop && watcher) {
+            ev_io_init (watcher, accept_cb, iSockListen, EV_READ);
+            ev_io_start (evloop, watcher);
+            puts ("press Ctrl-C to exit ...\n");
+            ev_run (evloop, 0);
 
-    puts ("press Ctrl-C to exit ...\n");
-
-    struct ev_loop *evloop = ev_loop_new (EVBACKEND_EPOLL);
-
-    ev_io ev_io_watcher;
-
-    ev_io_init (&ev_io_watcher, accept_callback,iSockListen, EV_READ);
-
-    ev_io_start (evloop,&ev_io_watcher); 
-    ev_loop (evloop,0);
-
-    ev_loop_destroy (evloop);
-    close (iSockListen);
+            ev_loop_destroy (evloop);
+            free (watcher);
+        } else 
+            puts ("-ERROR  small memory\n");
+    }
 
     puts ("... BY");
     return 0;
