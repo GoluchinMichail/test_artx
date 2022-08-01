ab2e33f8329a100984b00f41a8cad6ce6ed36bbc
GoluchinMichail <109588187+GoluchinMichail@users.noreply.github.com>
2022-08-01 15:27:26 +0300

    Update server__udp.c

--- test_3/test/server__udp.c
+++ test_3/test/server__udp.c
@@ -11,10 +11,7 @@ int main (int argc, char* argv[]) {
         return 1;
     }
 
-    // 1. Создать сокет
-
-    int sock = socket(AF_INET, SOCK_DGRAM , 0 );// SOCK_DGRAM означает UDP
-//    int sock = socket(AF_INET, SOCK_RAW , IPPROTO_UDP);
+    int sock = socket(AF_INET, SOCK_DGRAM , IPPROTO_UDP );
     if(sock < 0) {
         perror ("socket");
         return 2;
@@ -22,22 +19,17 @@ int main (int argc, char* argv[]) {
     printf("Socker: %d\n",sock);
 
 
-    // 2. Именованный сокет
-
     struct sockaddr_in local;
     memset (&local,0,sizeof(local));
     local.sin_family = AF_INET;
     local.sin_port = htons(atoi(argv[1]));
 
-    // 3. Привязать номер порта
     if( bind(sock, (struct sockaddr*)&local, sizeof(local)) < 0  ) {
         perror ("bind");
         return 3;
     }
 
     u_char buf [1024];// Буфер
-
-    // 4. Сервер получает информацию о клиенте
 
     while(1) {
         struct sockaddr_in client;
@@ -48,7 +40,6 @@ int main (int argc, char* argv[]) {
         if ( s > 0 ) {
             buf[s] = 0;
             printf ("%u bytes  %s port %d ==> %s\n", s, inet_ntoa(client.sin_addr), ntohs(client.sin_port), buf);
-            puts (&buf[20+8]);
         }
     }
 
